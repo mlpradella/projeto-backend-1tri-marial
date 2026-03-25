9d57fa6c5f9b35c4d531f02984ae52a90ca06afa
Bianca-a-a <bianca.gastaldim@yahoo.com>
2026-03-25 09:17:32 -0300

    header da pagina de receitas

---
+++ mobile/app/(tabs)/Receitas.tsx
@@ -0,0 +1,60 @@
+import { StyleSheet } from 'react-native';
+import ParallaxScrollView from '@/components/parallax-scroll-view';
+import { ThemedText } from '@/components/themed-text';
+import { ThemedView } from '@/components/themed-view';
+import { Fonts } from '@/constants/theme';
+
+export default function TabTwoScreen() {
+  return (
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: '#8C5B3E', dark: '#8C5B3E' }}
+      headerImage={
+        <ThemedView style={styles.headerContainer}>
+          <ThemedText
+            type="title"
+            style={{
+              fontFamily: Fonts.serif,
+              color: '#fff',
+              marginTop: 80,
+              backgroundColor: '#8C5B3E'
+            }}>
+            Doce Alquimia
+          </ThemedText>
+          <ThemedText
+            type="subtitle"
+            style={{
+              fontFamily: Fonts.mono,
+              color: '#fff',
+              marginTop: 30,
+              textAlign: 'center',
+              backgroundColor: '#8C5B3E',
+              fontSize: 15,
+            }}>
+            Onde ingredientes simples são transformados em algo valioso e especial
+          </ThemedText>
+        </ThemedView>
+      }>
+      <ThemedView style={styles.content}>
+        <ThemedText>
+          algum texto
+        </ThemedText>
+      </ThemedView>
+    </ParallaxScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    position: 'absolute',
+    top: 40,
+    left: 0,
+    right: 0,
+    alignItems: 'center',
+    paddingHorizontal: 16,
+    backgroundColor: '#8C5B3E'
+  },
+  content: {
+    marginTop: 16,
+    paddingHorizontal: 16,
+  },
+});
